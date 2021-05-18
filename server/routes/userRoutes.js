612277994180b6ecc5dbc649a582891141f1befa
fdavidsen <f.davidsen19@gmail.com>
2021-05-18 13:14:16 +0700

    Add admin page and move client to root directory

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {getUser, toggleFavoriteMusic} from "../controllers/controllers.js";
-
-const router = express.Router({mergeParams : true});
-
-router.patch("/", toggleFavoriteMusic);
-router.get("/", getUser);
-
-export {router};
